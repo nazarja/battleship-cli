8d8ca43fec86f588ef870e22acb4d6722d9c35e5
Sean Murphy <mail.seanmurphy@gmail.com>
2021-08-19 17:55:53 +0000

    add creds to heroku env vars

--- battleship/leaderboard.py
+++ battleship/leaderboard.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import List
 import os
+import json
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -14,9 +15,12 @@ class Leaderboard:
             'https://www.googleapis.com/auth/drive'
         ]
 
-        self.creds = os.environ.get('CREDS', Credentials.from_service_account_file('creds.json')) 
+        if os.environ.get('CREDS'):
+            self.creds = json.loads(os.environ.get('CREDS'))
+        else:
+            self.creds = Credentials.from_service_account_file('creds.json')
+
         self.scoped_creds = self.creds.with_scopes(self.scope)
         self.client = gspread.authorize(self.scoped_creds)
         self.sheet = self.client.open('Battleship')
-
         self.leaderboard: List[str] = self.sheet.worksheet('leaderboard').get_all_values()
